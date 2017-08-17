82c778e76c77f4b37e58d91f107c103193a49081
Moises Miguel <moisesmigueledu@gmail.com>
2017-08-16 19:04:25 -0500

    added bootstrap

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,28 @@
-console.log('It works!');
+import React from 'react';
+import {render} from 'react-dom';
+
+import {Header} from './Components/Header';
+import {Home} from './Components/Home';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-xs-10 col-xs-offset-1'>
+                        <Header/>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-xs-10 col-xs-offset-1'>
+                        <Home/>
+                    </div>
+                </div>
+            </div>
+        );//end of return
+    }
+}
+
+
+render(<App/>, document.getElementById('app'));
